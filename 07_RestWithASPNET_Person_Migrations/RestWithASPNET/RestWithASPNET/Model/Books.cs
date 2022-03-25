86b9ddddea35d025435f0143e9b2232c948586a5
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-03-25 14:27:27 -0300

    Testing New Controller - Book

---
+++ 07_RestWithASPNET_Person_Migrations/RestWithASPNET/RestWithASPNET/Model/Books.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET.Model
+{
+    [Table("books")]
+    public class Books
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("launch_time")]
+        public string Launch_Time { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+    }
+}
