886b000427a06dc33d7ac6a291f5ef34c27ad1aa
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-03-28 15:20:45 -0300

    Using GenericRepository in BookBusiness

--- 08_RestWithASPNET_Person_GenericRepository/RestWithASPNET/RestWithASPNET/Repository/Generic/IRepository.cs
+++ 08_RestWithASPNET_Person_GenericRepository/RestWithASPNET/RestWithASPNET/Repository/Generic/IRepository.cs
@@ -7,7 +7,7 @@ namespace RestWithASPNET.Repository
     public interface IRepository<T> where T : BaseEntity
     {
         T Create(T item);
-        T FindByID(long id);
+        T FindById(long id);
         List<T> FindAll();
         T Update(T item);
         void Delete(long id);
