9e6e47d8809cfc73a1ccf21071e9e0f47a1a8cce
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-03-31 08:42:34 -0300

    Implementing Class HypermediaLink

---
+++ 12_RestWithASPNET_Hateoas/RestWithASPNET/RestWithASPNET/Hypermedia/Abstract/ISupportHypermedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNET.Hypermedia.Abstract
+{
+    public interface ISupportHypermedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
