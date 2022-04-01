f2d7c9e81edfc443944650b959b2aceb9cebe0cd
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-04-01 11:23:06 -0300

    Adding Swagger Support

---
+++ 13_RestWithASPNET_Swagger/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Business;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Hypermedia.Filters;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [TypeFilter(typeof(HypermediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [TypeFilter(typeof(HypermediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [TypeFilter(typeof(HypermediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [TypeFilter(typeof(HypermediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 13_RestWithASPNET_Swagger/RestWithASPNET/RestWithASPNET/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,49 @@
+﻿using RestWithASPNET.Data.Converter.Contract;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Price = origin.Price,
+                launchDate = origin.launchDate,
+            };
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Price = origin.Price,
+                launchDate = origin.launchDate,
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
