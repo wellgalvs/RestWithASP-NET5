423fb2d9c73c7c58a2bae25e16799724b56bedfe
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-04-04 14:31:09 -0300

    Enabling Cors - Cross-origin Resource Sharing

---
+++ 14_RestWithASPNET_Cross_Origin_Resource_Sharing/RestWithASPNET/RestWithASPNET/Hypermedia/Abstract/ISupportsHypermedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNET.Hypermedia.Abstract
+{
+    public interface ISupportsHypermedia
+    {
+        List<HypermediaLink> Links { get; set; }
+    }
+}
---
+++ 14_RestWithASPNET_Cross_Origin_Resource_Sharing/RestWithASPNET/RestWithASPNET/Hypermedia/Filters/HypermediaFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Hypermedia.Filters
+{
+    public class HypermediaFilter : ResultFilterAttribute
+    {
+        private readonly HypermediaFilterOptions _hypermediaFilterOptions;
+
+        public HypermediaFilter(HypermediaFilterOptions hypermediaFilterOptions)
+        {
+            _hypermediaFilterOptions = hypermediaFilterOptions;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            TryEnrichResult(context);
+            base.OnResultExecuting(context);
+        }
+
+        private void TryEnrichResult(ResultExecutingContext context)
+        {
+            if (context.Result is OkObjectResult objectResult)
+            {
+                var enricher = _hypermediaFilterOptions
+                    .ContentResponseEnricherList
+                    .FirstOrDefault(x => x.CanEnrich(context));
+                if (enricher != null) Task.FromResult(enricher.Enrich(context));
+            }
+        }
+    }
+}
