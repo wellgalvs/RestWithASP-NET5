1c5b038215b4de9f8a7fb39aa2511a856359de3c
Wellington G. Alves da Silva <wellingtonalvs@gmail.com>
2022-04-05 10:50:57 -0300

    Creating UserRepository

---
+++ 15_RestWithASPNET_Authentication/RestWithASPNET/RestWithASPNET/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+    }
+}
---
+++ 15_RestWithASPNET_Authentication/RestWithASPNET/RestWithASPNET/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASPNET.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySqlContext _context;
+
+        public UserRepository(MySqlContext context)
+        {
+            _context = context;
+        }
+        public User ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
